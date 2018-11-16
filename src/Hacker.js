46bc6747cf62463c474020e9ca8907156ff23b26
Code Rebellion <coderebellion93@gmail.com>
2018-11-16 15:41:27 -0800

    Convert to HTTPS

--- src/Hacker.js
+++ src/Hacker.js
@@ -5,8 +5,8 @@ class Hacker extends Component {
   render() {
     return (
       <section id="hacker" align={this.props.alignment}>
-        <a href="http://hackertyper.net">HackerTyper.net</a><br /><br /><br />
-        <iframe src="http://hackertyper.net"></iframe>
+        <a href="https://hackertyper.net">HackerTyper.net</a><br /><br /><br />
+        <iframe src="https://hackertyper.net"></iframe>
         <p>While you're here, hack away!</p>
       </section>
     );
